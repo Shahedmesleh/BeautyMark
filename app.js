1e970b8eb7c2e7c1c487345989a4bc668c60bad8
Shahedmesleh <shmesleh123@gmail.com>
2021-02-08 13:00:49 +0200

    change in your page java script

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("class 4");
-var brandname =prompt("what the name of brand name");
+var brandname =prompt("what the name of brand name MAC or NARS");
 var number_of_prouduct = prompt("what the number of prouduct");
 var isbrandfound= prompt("is the brand found");
 console.log("brand name : " + brandname  + ',' + "number_of_prouduct : "+ number_of_prouduct +',' +
@@ -26,3 +26,24 @@ if (isbrandfound == true) {
 
 document.write("brand name : " + brandname  + ',' + "number_of_prouduct : "+ number_of_prouduct +',' +
 "brandisfound : " + isbrandfound);
+while(brandname!=="MAC"&& brandname!=="NARS")
+{
+  brandname=prompt("what the name of brand name MAC or NARS");
+}
+var itembrand='';
+if (brandname=="MAC"){
+  itembrand='<img src="image/m.jpg"/>';
+
+}
+else if (brandname=="NARS")
+{
+  itembrand='<img src="image/nars.jpg"/>';
+
+}
+var num_of_brand= prompt("what is number of brand");
+ var result ='';
+ for (var i=0; i<num_of_brand;i++){
+   result=result+itembrand;
+ }
+ 
+document.write(result);
